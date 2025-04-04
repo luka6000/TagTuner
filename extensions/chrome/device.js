262d763d83f4a02ae34f7bc3ff5faea8dfd77341
Florian Necas <florian.necas@camptocamp.com>
2025-04-04 18:49:10 +0200

    feat: add configurable device url and improve UX

--- extensions/chrome/device.js
+++ extensions/chrome/device.js
@@ -3,27 +3,20 @@ function getButtonUrl(baseUrl, field,) {
 }
 
 function getInputUrl(baseUrl, field, value) {
-    const inputUrl = `${baseUrl}/text/${field}/set?value=${encodeURIComponent(value)}`;
-    console.log(inputUrl)
-    return inputUrl;
+    return `${baseUrl}/text/${field}/set?value=${encodeURIComponent(value)}`;
 }
 
-function callDeviceWrite(artist, playlistOrAlbumTitle, url) {
-    const baseUrl = "http://192.168.1.199"
-    return Promise.all([
-        fetch(getInputUrl(baseUrl, 'playlist_uri', url), {"method": "POST"}),
-        fetch(getInputUrl(baseUrl, 'playlist_artist', artist), {"method": "POST"}),
-        fetch(getInputUrl(baseUrl, 'playlist_name_or_album_title', playlistOrAlbumTitle), {"method": "POST"})
-    ]).then(() => {
-        return fetch(getButtonUrl(baseUrl, 'write_tag'), {"method": "POST"}).then(() => {
-            return true
-        });
-    }).catch(error => {
-        console.error('Error:', error);
-        return fetch(getButtonUrl(baseUrl, 'cancel_writing'), {"method": "POST"}).then(() => {
-            return false
-        });
-    });
+async function callDeviceWrite(baseUrl, artist, playlistOrAlbumTitle, url) {
+    try {
+        await fetch(getInputUrl(baseUrl, 'playlist_uri', url), {"method": "POST"});
+        await fetch(getInputUrl(baseUrl, 'playlist_artist', artist), {"method": "POST"});
+        await fetch(getInputUrl(baseUrl, 'playlist_name_or_album_title', playlistOrAlbumTitle), {"method": "POST"});
+        await fetch(getButtonUrl(baseUrl, 'write_tag'), {"method": "POST"});
+        return true;
+    } catch (error) {
+        console.log("Error in call device write:", error);
+        return false;
+    }
 }
 
 export default callDeviceWrite
--- extensions/chrome/tagtuner-device.js
+++ extensions/chrome/tagtuner-device.js
@@ -1,11 +1,78 @@
+import getSpotifyData from './spotify.js';
+import callDeviceWrite from "./device.js";
+
 document.getElementById('deviceSetButton').addEventListener('click', async () => {
-    const device_url = document.getElementById("device_url").value;
+    const device_url = document.getElementById("device_url_input").value;
     await chrome.storage.local.set({
-        target_ha: device_url
-    }).then(async () => {
-        await chrome.action.setPopup({popup: ''});
+        target_device: device_url
     }).then(() => {
         window.close()
     });
 });
 
+document.getElementById('clearDeviceLink').addEventListener('click', async () => {
+    chrome.storage.local.remove('target_device', () => {
+        console.log("Storage cleared")
+        window.close()
+    });
+})
+
+
+function showElement(el, show) {
+    document.getElementById(el).style.display = show ? "block" : "none";
+}
+
+function clearAndClose(interval) {
+    showElement("loading", false)
+    clearInterval(interval)
+    setTimeout(() => {
+        window.close()
+    }, 1500)
+}
+
+let baseUrl = "";
+window.onload = async function () {
+    const [currentTab] = await chrome.tabs.query({active: true, currentWindow: true});
+    chrome.storage.local.get('target_device', async (localStorage) => {
+        if (localStorage.target_device) {
+            baseUrl = localStorage.target_device;
+            document.getElementById("device_url").textContent = baseUrl;
+            showElement("load-div", true)
+            await chrome.scripting.executeScript({
+                target: {tabId: currentTab.id, allFrames: true},
+                func: getSpotifyData,
+                args: [currentTab.url]
+            }).then(([res]) => {
+                callDeviceWrite(baseUrl, res.result.artist, res.result.albumOrPlaylistTitle, res.result.url).then((success) => {
+                    if (success) {
+                        const i = setInterval(async () => {
+                            await fetch(baseUrl + "/binary_sensor/_writing_", { signal: AbortSignal.timeout(1000) })
+                                .catch((err) => {
+                                    if (err.name === "TimeoutError") {
+                                        console.error("Timeout: It took more than 5 seconds to get the result!");
+                                        setTimeout(() => {
+                                            showElement("success", true);
+                                            clearAndClose(i)
+                                        }, 4000)
+                                    } else {
+                                        clearAndClose(i)
+                                        showElement("error", true)
+                                        console.error('Error:', error);
+                                    }
+                                })
+                        }, 500)
+                        setTimeout(() => {
+                            clearAndClose(i)
+                        }, 20000)
+                    } else {
+                        showElement("loading", false)
+                        showElement("error", true)
+
+                    }
+                })
+            });
+        } else {
+            showElement("set-url-div", true)
+        }
+    });
+}
